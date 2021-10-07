30368eb0eec0a90e30ce2c6e3ec1189f277f1433
Emilaberg <emil.aberg@live.com>
2021-10-07 11:44:42 +0200

    projectsupdate

---
+++ script.js
@@ -0,0 +1,20 @@
+function myFunction(){
+    var x = document.getElementById("links");
+    
+    if (x.style.display === "block"){
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  document.querySelectorAll("#links").forEach(item =>{
+    item.addEventListener("click", event =>{
+      close();
+    })
+
+  })
+  function close(){
+    var x = document.getElementById("links");
+    x.style.display = "none";
+  }
